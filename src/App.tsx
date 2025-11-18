b2d2023c8c5c0010db16e925a91947c1b59dce25
Cedric Ko <yacmov@gmail.com>
2025-11-18 11:19:58 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar"
+
+const App = () => {
+  return (
+    <main className="text-3xl font-bold underline text-indigo-700">
+      <NavBar/>
+    </main>
+  )
+}
+
+export default App
